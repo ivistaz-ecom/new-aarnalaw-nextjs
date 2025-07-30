49754f4432e76cf0b7a05ead2853d417fc1a22b2
sibiraj-002 <shansibi2002@gmail.com>
2025-07-30 17:30:26 +0530

    New banner changed  home page

--- components/HomePage/Banner.js
+++ components/HomePage/Banner.js
@@ -17,16 +17,18 @@ export default function Banner() {
   const isFirstBanner = currentIndex === 0;
 
   return (
-    <div className="relative h-[70vh] w-full lg:h-screen">
+    <div className="relative w-full">
       <h1 className="sr-only">Client-Centric Problem Solving</h1>
 
       {/* Mobile */}
-      <div className="relative w-full h-[70vh] aspect-[414/500] lg:hidden">
+      <div className="relative lg:hidden">
         <Image
           src={banner.mobileBannerUrl}
           alt={banner.bannerText}
-          fill
-          style={{ objectFit: 'cover' }}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }}
           loading={isFirstBanner ? "eager" : "lazy"}
           priority={isFirstBanner}
           placeholder="blur"
@@ -39,19 +41,21 @@ export default function Banner() {
       </div>
 
       {/* Desktop */}
-      <div className="relative w-full h-screen aspect-[1335/940] hidden lg:block">
+      <div className="relative hidden lg:block">
         <Image
           src={banner.bannerUrl}
           alt={banner.bannerText}
-          fill
-          style={{ objectFit: 'cover' }}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }}
           loading={isFirstBanner ? "eager" : "lazy"}
           priority={isFirstBanner}
           placeholder="blur"
-          blurDataURL={banner.bannerUrl}
+          blurDataURL={banner.bannerUrl} 
         />
-        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center p-4 text-center text-white">
-          <p className="text-4xl font-bold lg:text-5xl">{banner.bannerText}</p>
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center p-4 text-center text-white pt-[200px]">
+          <p className="text-4xl font-bold lg:text-4xl">{banner.bannerText}</p>
           <p className="py-8 text-xl lg:w-7/12">{banner.bannerPara}</p>
         </div>
       </div>
--- utils/homebanner-data.js
+++ utils/homebanner-data.js
@@ -1,19 +1,19 @@
 export const homeBanner = [
   {
-    bannerUrl: "/banner/new_desktop_home_banner_2.webp",
+    bannerUrl: "/banner/banner1.jpg",
     mobileBannerUrl: "/banner/new_mobile_home_banner_02.webp",
     bannerText: "Client-Centric Problem Solving",
     bannerPara:
       "We are counsel-led Indian legal litigation boutique firm, representing India and people across the world in diverse areas of expertise.",
   },
   {
-    bannerUrl: "/banner/new_desktop_home_banner_1.webp",
+    bannerUrl: "/banner/banner2.jpg",
     mobileBannerUrl: "/banner/new_mobile_home_banner_01.webp",
     bannerText: "Rooted in India, Global in Practice ",
     bannerPara: "",
   },
   {
-    bannerUrl: "/banner/new_desktop_home_banner_3.webp",
+    bannerUrl: "/banner/banner3.jpg",
     mobileBannerUrl: "/banner/new_mobile_home_banner_03.webp",
     bannerText: "Unlocking Abundance",
     bannerPara: "",
