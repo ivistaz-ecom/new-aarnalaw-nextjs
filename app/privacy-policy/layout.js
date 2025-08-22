1405dd99c344fcd1afd50c1df8829b70bc10489a
sibiraj-002 <shansibi2002@gmail.com>
2025-08-22 16:47:29 +0530

    privacy polict page metas updated

--- app/privacy-policy/layout.js
+++ app/privacy-policy/layout.js
@@ -1,22 +1,20 @@
 export const metadata = {
-    title: "privacy-policy | Law Firm in India - Aarna Law",
+  title: "Privacy Policy | Law Firm in India - Aarna Law",
+  description:
+    "Read Aarna Law's privacy policy to understand how we collect, use, and protect your personal data. Learn about your rights and our commitment to safeguarding your privacy.",
+  metadataBase: new URL("https://www.aarnalaw.com"),
+  alternates: {
+    canonical: "/privacy-policy",
+  },
+  openGraph: {
+    title: "Privacy Policy | Law Firm in India - Aarna Law",
     description:
-      "Get in touch with the internationally recognised law firm in India - Aarna Law. privacy-policy now for professional legal assistance.",
-    metadataBase: new URL("https://www.aarnalaw.com"),
-    alternates: {
-      canonical: "/privacy-policy",
-    },
-    openGraph: {
-      title: "privacy-policy | Law Firm in India - Aarna Law",
-      description:
-        "Get in touch with the internationally recognised law firm in India - Aarna Law. privacy-policy now for professional legal assistance.",
-      url: "/privacy-policy",
-      images: "/aarnalaw_new_banner.jpg",
-    },
-  };
-  
-  export default function RootLayout({ children }) {
-    return <>{children}</>;
-  }
-  
-  // we need to change title and description
+      "Read Aarna Law's privacy policy to understand how we collect, use, and protect your personal data. Learn about your rights and our commitment to safeguarding your privacy.",
+    url: "/privacy-policy",
+    images: "/aarnalaw_new_banner.jpg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return <>{children}</>;
+}
